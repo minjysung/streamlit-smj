d237cb863f316a39e4257ca2c182184b855b5236
minjysung <123149592+minjysung@users.noreply.github.com>
2023-01-20 17:45:00 +0900

    Update smj07.py

--- smj07.py
+++ smj07.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title(':money_with_wings: **_:green[MONEY]_** :money_with_wings:')
+st.title('$huge :money_with_wings: **_:green[MONEY]_** :money_with_wings:')
 
 
 import streamlit as st
